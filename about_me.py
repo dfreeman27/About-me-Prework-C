20263e80bb0699b9f5f2a6297435b9978b4e8681
DJ Freeman <dajawaunfreeman27@gmail.com>
2026-06-26 15:39:12 -0500

    Add files via upload

---
+++ about_me.py
@@ -0,0 +1,35 @@
+
+# Variable user info 
+from operator import add
+
+
+first_name = "Dajawaun" 
+last_name = "Freeman"
+age = 28
+city = "St.Louis"
+Occupation = "Truck Driver"
+Why_I_am_learning_to_code = "I want to learn how to code because I want to be able to create my own applications and websites. I also want to be able to understand how technology works and how it can be used to solve problems."
+fun_fact = "I am an artist"
+
+print("=" * 40)
+print("Personal Introduction")
+print("=" * 40)
+
+# output for user info
+
+print("Name:", first_name,last_name)
+print("Age:", age)
+print("City:", city)
+print("Occupation:", Occupation)
+print("Why I am learning to code:", Why_I_am_learning_to_code)
+print("Fun Fact:", fun_fact)
+
+print("=" * 40)
+
+# This is a simple program that outputs personal information about the user. It includes variables for the user's first name, last name, age, city, occupation, reason for learning to code, and a fun fact. The program then prints this information in a formatted manner.
+
+
+
+
+
+
